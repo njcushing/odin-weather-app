40fc8eb06b2142a48e3128cbe83747d4eeaae079
njcushing <129661299+njcushing@users.noreply.github.com>
2023-06-27 19:54:30 +0100

    Create function to extract relative display info from JSON

--- src/index.js
+++ src/index.js
@@ -10,10 +10,29 @@ const getWeather = async (location) => {
         );
         if (!weatherResponse.ok) throw new Error("Weather response is not ok.");
         const weatherJSON = await weatherResponse.json();
-        return weatherJSON;
+        return getRelevantWeatherInformationFromJSON(weatherJSON);
     } catch (error) {
         console.log(`Error! ${error.message}`);
     }
 };
+
+const getRelevantWeatherInformationFromJSON = (weatherJSON) => ({
+    countryName: weatherJSON.location.country,
+    cityName: weatherJSON.location.name,
+    region: weatherJSON.location.region,
+    localTime: weatherJSON.location.localtime,
+    tempc: weatherJSON.current.temp_c,
+    tempf: weatherJSON.current.temp_f,
+    humidity: weatherJSON.current.humidity,
+    windDeg: weatherJSON.current.wind_degree,
+    windDir: weatherJSON.current.wind_dir,
+    windSpeedMph: weatherJSON.current.wind_mph,
+    windSpeedKph: weatherJSON.current.wind_kph,
+    precipInch: weatherJSON.current.precip_in,
+    precipMm: weatherJSON.current.precip_mm,
+    conditionText: weatherJSON.current.condition.text,
+    conditionIcon: weatherJSON.current.condition.icon,
+    lastUpdated: weatherJSON.current.last_updated,
+});
 
 getWeather("london");
