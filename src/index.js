e30702cae016d66c95ec72f2c3f825372c7a0aa8
njcushing <129661299+njcushing@users.noreply.github.com>
2023-06-27 19:38:39 +0100

    Create function that fetches location weather data from WeatherAPI

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./styles.css";
+
+const APIKey = "defd47dda70843cfaa084416232506";
+
+const getWeather = async (location) => {
+    try {
+        const weatherResponse = await fetch(
+            `https://api.weatherapi.com/v1/current.json?key=${APIKey}&q=${location}`,
+            { mode: "cors" }
+        );
+        if (!weatherResponse.ok) throw new Error("Weather response is not ok.");
+        const weatherJSON = await weatherResponse.json();
+        return weatherJSON;
+    } catch (error) {
+        console.log(`Error! ${error.message}`);
+    }
+};
+
+getWeather("london");
