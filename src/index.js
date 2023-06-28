791c9df4d28a59950d256c908effaeba27fe4a41
njcushing <129661299+njcushing@users.noreply.github.com>
2023-06-28 18:39:20 +0100

    Add some basic styling to the weather app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "./styles.css";
 import weatherApp from "./weather-app.js";
+import weatherAppStyles from "./weather-app-styles-default.lazy.css";
 
 const body = document.querySelector("body");
 const newWeatherApp = weatherApp(body);
+
+weatherAppStyles.use();
